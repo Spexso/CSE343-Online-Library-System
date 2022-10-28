1e45ebf0d4226aa8a3bedcae51e76bcc3876467f
Ömer Faruk Çolakel <74856597+OmerFColakel@users.noreply.github.com>
2022-10-28 16:56:21 +0300

    description

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'pages/login_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  final bool value = true;
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(useMaterial3: true, colorSchemeSeed: Colors.orange),
+      home: const LoginPage(),
+    );
+  }
+}
