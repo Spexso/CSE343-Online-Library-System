527ea3b9c0d2a8b5c2ef574cafc29f0f6996254e
Kağan <ipekipek48@hotmail.com>
2023-01-04 15:49:45 +0300

    Reactapp name Changed to Libware, new icon added

--- web/src/components/Booklist.js
+++ web/src/components/Booklist.js
@@ -5,12 +5,30 @@ import Board from "./Board";
 
 const Booklist = () => {
     // eslint-disable-next-line
-    const [books, setBooks] = useState([
+    const [search,setSearch]=useState("");
+    const {REACT_APP_API_TOKEN} = process.env;
+    const searchBook=(evt)=> {
+        if(evt.key === "Enter")
+        {
+            console.log("Test");
+            /*fetch(`${REACT_APP_API_TOKEN}/guest/admin-login`, {
+                method: 'POST',
+                headers: { "Content-Type": "application/json"},
+                body: JSON.stringify({
+                    name: ,
+                    password: ,
+                }),
+            }).then( res =>console.log(res))
+              .catch(err=>console.log(err))
+            */    
+        }
+    }
+    /*const [books, setBooks] = useState([
         { title: '1984', body: 'lorem ipsum...', author: 'mario', id: 1 },
         { title: 'Kurak Günler', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
         { title: 'Rüyaların Yorumu', body: 'lorem ipsum', author: 'Freud', id: 3 }
     ]);
-    
+    */
 
     return(  
         <>
@@ -21,7 +39,9 @@ const Booklist = () => {
                 <div className="row2">
                     <h2>Search for book</h2>
                     <div className="search">
-                        <input type="text" placeholder="Enter Information" />
+                        <input type="text" placeholder="Enter Information" 
+                         value={search} onChange={e=>setSearch(e.target.value)}
+                         onKeyDown={searchBook}/>
                         <button><i class="fas fa-search"></i> </button>
                     </div>
                     <img src={background} alt="Background of Search Page" />
